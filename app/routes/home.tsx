48c068ca81bef1c32e3a0d6dc623b32dba7147de
Syed Aamina <aaminasyed04@gmail.com>
2026-03-03 01:48:46 +0530

    feat: add resume card section and constants

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
-
-
+import { resumes } from "constants/index";
+import ResumeCard from "~/components/ResumeCard";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "ResuScan AI" },
@@ -10,13 +10,24 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-  <Navbar/>
-  <section className="main-section">
-    <div className="page-heading">
-      <h1>Track your Applications & Resume Ratings</h1>
-      <h2>Review your submissions and get AI powered feedback!</h2>
-    </div>
-  </section>
-  </main>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-10">
+          <h2 className="font-bold">Track your Applications & Resume Ratings</h2>
+          <h3>Review your submissions and get AI powered feedback!</h3>
+        </div>
+        {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+      </section>
+
+      
+    </main>
+  );
 }
